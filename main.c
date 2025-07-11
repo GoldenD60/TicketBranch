38b171964b5386b9580989f7235f67710c801221
David Skillman <dskillman@silhouettesoftware.com>
2025-07-11 13:50:15 +0100

    Complete all functions

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <time.h>
 #include <string.h>
@@ -43,15 +44,13 @@ void set_console() {}
 #define ANSI_STYLE_UNDERLINE    "\x1b[4m"
 
 #define NONE_PLACEHOLDER		"<none>"
-
-typedef enum Command {
-	CMD_HELP,
-	CMD_VERSION,
-	CMD_CONFIG,
-	CMD_NEW,
-	CMD_COMMIT,
-	CMD_SETFORMAT
-} Command;
+	
+#define ERR_NO_INITIALS			1
+#define ERR_NO_DEFAULT_BRANCH	2
+#define ERR_NO_BRANCH_FORMAT	4
+#define ERR_NO_TICKET			8
+#define ERR_MISSING_ARG			16
+#define ERR_CANCELLED			32
 
 typedef struct {
 	int version;
@@ -73,14 +72,34 @@ int help() {
 		ANSI_RESET_ALL ANSI_STYLE_BOLD"usage: %s [-v | --version] [-h | --help] <command> [<args>]\n"ANSI_RESET_ALL
 		"\n"
 		ANSI_STYLE_BOLD"commands:\n"ANSI_RESET_ALL
+		"    -h, --help                         Show this help message\n"
+		"    -v, --version                      Show version information\n"
+		"    config [-r | --reset]              Show or set the configuration for the current user.\n"
+		"    new <ticket>                       Creates a new branch using the ticket number, by checking out\n"
+		"                                       the current branch and creating a new one.\n"
+		"    commit <message>                   Commits any changes made to the repository, using the message\n"
+		"                                       content and recorded time.\n"
+		"    format [-c | --commit <format>]    Sets the format for the branch name or commit message.\n"
+		"           [-b | --branch <format>]\n",
+		name
+	);
+	return 0;
+}
+
+int help_config() {
+	printf(
+		ANSI_RESET_ALL ANSI_STYLE_BOLD"usage: %s config\n"ANSI_RESET_ALL
+		"\n"
+		ANSI_STYLE_BOLD"description: "ANSI_RESET_ALL"Show or set the configuration for the current user.\n"
+		"\n"
+		ANSI_STYLE_BOLD"options:\n"ANSI_RESET_ALL
 		"    -h, --help                 Show this help message\n"
-		"    -v, --version              Show version information\n"
-		"    config                     Show or set the configuration for the current user.\n"
-		"    new <ticket>               Creates a new branch using the ticket number, by checking out\n"
-		"                               the current branch and creating a new one.\n"
-		"    commit <message>           Commits any changes made to the repository, using the message\n"
-		"                               content and recorded time.\n"
-		"    setformat                  Sets the format for the branch name or commit message.\n",
+		"    -r, --reset                Reset all configuration\n"
+		"\n"
+		ANSI_STYLE_BOLD"configuration options:\n"ANSI_RESET_ALL
+		"    Default Branch             The default branch to checkout before creating a new branch.\n"
+		"    Initials                   The initials of your name to use in branch names and commit messages.\n"
+		"    Repository Name            The name of the repository, used in branch names.\n",
 		name
 	);
 	return 0;
@@ -103,20 +122,15 @@ int help_new() {
 	return 0;
 }
 
-int help_config() {
+int help_commit() {
 	printf(
-		ANSI_RESET_ALL ANSI_STYLE_BOLD"usage: %s config\n"ANSI_RESET_ALL
+		ANSI_RESET_ALL ANSI_STYLE_BOLD"usage: %s commit <message>\n"ANSI_RESET_ALL
 		"\n"
-		ANSI_STYLE_BOLD"description: "ANSI_RESET_ALL"Show or set the configuration for the current user.\n"
+		ANSI_STYLE_BOLD"description: "ANSI_RESET_ALL"Commits any changes made to the repository, using the message\n"
+		"             content and recorded time.\n"
 		"\n"
 		ANSI_STYLE_BOLD"options:\n"ANSI_RESET_ALL
-		"    -h, --help                 Show this help message\n"
-		"    -r, --reset                Reset all configuration\n"
-		"\n"
-		ANSI_STYLE_BOLD"configuration options:\n"ANSI_RESET_ALL
-		"    Default Branch             The default branch to checkout before creating a new branch.\n"
-		"    Initials                   The initials of your name to use in branch names and commit messages.\n"
-		"    Repository Name            The name of the repository, used in branch names.\n",
+		"    -h, --help                 Show this help message\n",
 		name
 	);
 	return 0;
@@ -169,69 +183,94 @@ int load_config() {
 	return 1;
 }
 
-void format(
-		const char *fmt,
-		char *out,
-		const char *n,		// %n
-		const char *i, 		// %i   
-		const char *r,		// %r
-		const char *t,		// %t
-		const char *m,		// %m
-		const char *b) {	// %b
-    const char *p = fmt;
-    char *o = out;
+int format(char *out, size_t out_size,
+		   const char *fmt,
+		   const char *ticket,
+		   const char *initials,
+		   const char *repo,
+		   const char *time,
+		   const char *message,
+		   const char *branch) {
+    size_t i = 0, j = 0;
 
-    while (*p) {
-        if (*p == '%' && *(p+1)) {
-            p++;
-            switch (*p) {
-                case 'n':
-                    while (*n) *o++ = *n++;
-                    break;
-                case 'i':
-                    while (*i) *o++ = *i++;
-                    break;
-                case 'r':
-                    while (*r) *o++ = *r++;
-                    break;
-                case 't':
-                    while (*t) *o++ = *t++;
-                    break;
-                case 'm':
-                    while (*m) *o++ = *m++;
-                    break;
-                case 'b':
-                    while (*b) *o++ = *b++;
-                    break;
-                case '%':
-                    *o++ = '%';
-                    break;
-                default:
-                    *o++ = '%';
-                    *o++ = *p;
+    while (fmt[i] && j < out_size - 1) {
+        if (fmt[i] == '%' && fmt[i + 1]) {
+            i++;
+            const char *rep = NULL;
+            switch (fmt[i]) {
+                case 'n': rep = ticket; break;
+                case 'i': rep = initials; break;
+                case 'r': rep = repo; break;
+                case 't': rep = time; break;
+                case 'm': rep = message; break;
+                case 'b': rep = branch; break;
+                case '%': out[j++] = '%'; rep = NULL; break;
+                default:  out[j++] = '%'; out[j++] = fmt[i]; rep = NULL; break;
             }
-            p++;
+            if (rep) while (*rep && j < out_size - 1) out[j++] = *rep++;
         } else {
-            *o++ = *p++;
+            out[j++] = fmt[i];
         }
+        i++;
     }
-    *o = '\0';
+
+    out[j] = '\0';
+    return 0;
 }
 
-void format_cfg(const char *fmt, char *out, char *message, char *branch) {
-	format(
-		fmt,
+char *join_args(int argc, char **argv, int start) {
+    if (argc <= start) return strdup("");
+
+    int len = 0;
+    for (int i = start; i < argc; i++) len += strlen(argv[i]) + 1;
+
+    char *s = malloc(len), *p = s;
+    for (int i = start; i < argc; i++)
+        p += sprintf(p, "%s%s", argv[i], i < argc - 1 ? " " : "");
+    
+    return s;
+}
+
+int format_cfg(char *out, size_t out_size, const char *fmt, char *message, char *branch) {
+	char smins[16];
+	itoa((int)((time(NULL) - cfg.timer) / 60), smins, 10);
+	return format(
 		out,
+		out_size,
+		fmt,
 		cfg.ticket,
 		cfg.initials,
 		cfg.repository_name,
-		"Time unknown",
+		smins,
 		message,
 		branch
 	);
 }
 
+int check_cfg() {
+	int err = 0;
+	if (!strcmp(cfg.initials, "")) {
+		printf("You need to set your initials in the configuration\n");
+		err |= ERR_NO_INITIALS;
+	}
+	if (!strcmp(cfg.default_branch, "")) {
+		printf("You need to set the default branch in the configuration\n");
+		err |= ERR_NO_DEFAULT_BRANCH;
+	}
+	if (!strcmp(cfg.branch_format, "")) {
+		printf("You need to set the branch format in the format configuration.\nUse '%s format [-b | --branch] <format>'\n", name);
+		err |= ERR_NO_BRANCH_FORMAT;
+	}
+	if (!strcmp(cfg.commit_format, "")) {
+		printf("You need to set the commit format in the format configuration.\nUse '%s format [-c | --commit] <format>'\n", name);
+		err |= ERR_NO_BRANCH_FORMAT;
+	}
+	return err;
+}
+
 int ask() {
+	printf("Is this OK? (Y/n) ");
+
     char answer = '\0';
     while (answer != 'y' && answer != 'n') {
         answer = getchar();
@@ -281,23 +320,115 @@ int config() {
 	return 0;
 }
 
+int git(char *command) {
+	system(command);
+	return 0;
+}
+
 int new(char ticket[8])
 {
-	char new_branch[255];
+	char new_branch[255] = "";
 	
 	strncpy(cfg.ticket, ticket, sizeof(cfg.ticket));
-	format_cfg(cfg.branch_format, new_branch, NONE_PLACEHOLDER, NONE_PLACEHOLDER);
+	format_cfg(new_branch, sizeof(new_branch), cfg.branch_format, NONE_PLACEHOLDER, NONE_PLACEHOLDER);
 
-	printf("Will checkout branch %s and create new branch %s.\nIs this OK? (Y/n) ", cfg.default_branch, new_branch);
+	printf("Will checkout branch %s and create new branch %s.\n", cfg.default_branch, new_branch);
 	if (!ask()) {
 		printf(ANSI_COLOR_RED ANSI_STYLE_BOLD "Cancelled" ANSI_RESET_ALL);
-		return 1;
+		return ERR_CANCELLED;
 	}
 
-	printf("git fetch\n");
-	printf("git checkout %s\n", cfg.default_branch);
-	printf("git checkout -b %s\n", new_branch);
+	char command[1024] = "";
 
+	sprintf(command, "git fetch\n");
+	git(command);
+
+	sprintf(command, "git checkout %s\n", cfg.default_branch);
+	git(command);
+
+	sprintf(command, "git checkout -b %s\n", new_branch);
+	git(command);
+
+	cfg.timer = time(NULL);
+	save_config();
+	return 0;
+}
+
+int commit(int argc, char **argv) {
+	int mins = (int)((time(NULL) - cfg.timer) / 60);
+
+	printf("Time to commit: %i minute%s\n", mins, mins == 1 ? "" : "s");
+	if (!ask()) {
+		printf("Enter time (minutes): ");
+		scanf("%i", &mins);
+	}
+
+    int flagc = 0;
+    for (int i = 2; i < argc; i++) {
+		flagc = i;
+        if (argv[i][0] != '-') break;
+    }
+
+    char *flagstr = join_args(flagc, argv, 2);
+    char *argstr  = join_args(argc, argv, flagc);
+
+	char branch[255] = "";
+	format_cfg(branch, sizeof(branch), cfg.branch_format, NONE_PLACEHOLDER, NONE_PLACEHOLDER);
+
+	char message[72] = "";
+	format_cfg(message, sizeof(message), cfg.commit_format, argstr, branch);
+	free(argstr);
+
+	printf("\nThe commit message will be: \"%s\"\n", message);
+	if (!ask()) {
+		printf(ANSI_COLOR_RED ANSI_STYLE_BOLD "Cancelled" ANSI_RESET_ALL);
+		return ERR_CANCELLED;
+	}
+
+	char command[1024] = "";
+	sprintf(command, "git commit %s -m \"%s\"\n", flagstr, message);
+	git(command);
+	free(flagstr);
+
+	return 0;
+}
+
+int setformat(int argc, char **argv) {
+	int branch = 0, commit = 0;
+	for (int i = 2; i < argc; i++)
+	{
+		if (!strcmp(argv[i], "-b") || !strcmp(argv[i], "--branch"))
+			branch = i + 1;
+		else if (!strcmp(argv[i], "-c") || !strcmp(argv[i], "--commit"))
+			commit = i + 1;
+	}
+	
+	int branchc = commit > branch ? commit - 1 : argc;
+	int commitc = branch > commit ? branch - 1 : argc;
+
+	if (commit)
+	{
+		if (commit == commitc)
+			printf("%s\n", cfg.commit_format);
+		else {
+			char *str = join_args(commitc, argv, commit);
+			strncpy(cfg.commit_format, str, sizeof(cfg.commit_format));
+			free(str);
+		}
+	}
+	if (branch) {
+		if (branch == branchc)
+			printf("%s\n", cfg.branch_format);
+		else {
+			char *str = join_args(branchc, argv, branch);
+			strncpy(cfg.branch_format, str, sizeof(cfg.branch_format));
+			free(str);
+		}
+	}
+	if (!branch && !commit)
+	{
+		return printf("Commit Message Format: %s\nBranch Format: %s\n", cfg.commit_format, cfg.branch_format);
+	}
 	save_config();
 	return 0;
 }
@@ -305,8 +436,8 @@ int new(char ticket[8])
 void set_default_config() {
 	memset(&cfg, 0, sizeof(cfg));
 	cfg.version = 1;
-	strcpy(cfg.branch_format, "b1.0-%i-%n");
-	cfg.timer = -1;
+	strncpy(cfg.branch_format, "", sizeof(cfg.branch_format));
+	strncpy(cfg.commit_format, "", sizeof(cfg.commit_format));
 	save_config();
 }
 
@@ -355,26 +486,14 @@ int main(int argc, char **argv)
 			
 			if (argc < 3) {
 				printf("Ticket number required\n");
-				return help_new();
+				return help_new() | ERR_MISSING_ARG;
 			}
 			if (!strcmp(argv[2], "-h") || !strcmp(argv[2], "--help"))
 			{
 				return help_new();
 			}
 
-			int err = 0;
-			if (!strcmp(cfg.initials, "")) {
-				printf("You need to set your initials in the configuration\n");
-				err = 1;
-			}
-			if (!strcmp(cfg.default_branch, "")) {
-				printf("You need to set the default branch in the configuration\n");
-				err = 1;
-			}
-			if (!strcmp(cfg.branch_format, "")) {
-				printf("You need to set the branch format in the format configuration\n");
-				err = 1;
-			}
+			int err = check_cfg();
 			printf(ANSI_RESET_ALL);
 
 			if (err) return err;
@@ -382,11 +501,29 @@ int main(int argc, char **argv)
 		}
 		else if (!strcmp(argv[1], "commit"))
 		{
-			return 0;
+			printf(ANSI_COLOR_RED ANSI_STYLE_BOLD);
+			if (argc < 3) {
+				printf("Commit message required\n");
+				return help_commit() | ERR_MISSING_ARG;
+			}
+			if (!strcmp(argv[2], "-h") || !strcmp(argv[2], "--help"))
+			{
+				return help_commit();
+			}
+
+			int err = check_cfg();
+			if (!strcmp(cfg.ticket, "")) {
+				printf("You are not in a ticket branch");
+				err |= ERR_NO_TICKET;
+			}
+			printf(ANSI_RESET_ALL);
+
+			if (err) return err;
+			return commit(argc, argv);
 		}
-		else if (!strcmp(argv[1], "setformat"))
+		else if (!strcmp(argv[1], "format"))
 		{
-			return 0;
+			return setformat(argc, argv);
 		}
 	}
 	printf(ANSI_COLOR_RED ANSI_STYLE_BOLD"Invalid command\n");
